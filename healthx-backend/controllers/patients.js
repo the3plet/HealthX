df6a2ef50d35a279615a77dcbc87eeea271422a8
shinjith-dev <shinjithpallikkara@gmail.com>
2022-11-19 18:46:35 +0530

    patient data

--- healthx-backend/controllers/patients.js
+++ healthx-backend/controllers/patients.js
@@ -1,17 +1,30 @@
-const patientsRouter = require('express').Router()
-const Patient = require('../models/patient')
+const patientsRouter = require("express").Router();
+const Patient = require("../models/patient");
 
-patientsRouter.get('/info',(req,res)=>{
-    res.set('content-type','text/html')
-    res.send(`<p>Many people present</p>`)
+patientsRouter.get("/info", (req, res) => {
+  res.set("content-type", "text/html");
+  res.send(`<p>Many people present</p>`);
+});
+
+patientsRouter.get("/:id", (req, res) => {
+Patient.findById(req.params.id).then(patient=>{
+    res.json(patient)
 })
+});
 
-patientsRouter.get('/:id',(req,res)=>{
-    res.send(`Patient-${req.params.id}`)
-})
 
-patientsRouter.post('/',(req,res)=>{
-const body = req.body;
-})
 
-module.exports = patientsRouter
+patientsRouter.post("/", (req, res) => {
+  const body = req.body;
+  const patient = new Patient({
+    name: body.name,
+    age: body.age,
+  });
+
+  patient
+    .save()
+    .then((addedPatient) => res.json(addedPatient))
+    .catch();
+});
+
+module.exports = patientsRouter;
--- healthx-backend/models/patient.js
+++ healthx-backend/models/patient.js
@@ -1,14 +1,35 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const patientSchema = new mongoose.Schema({
-    name: String,
-    Sex: String,
-    Age: Number,
-    Height: Number,
-    Weight: Number,
-    BP: Number,
-    Smocking: Boolean,
-    Alchohol:Boolean
-})
+  name: String,
+  Age: Number,
+  // Sex: String,
+  //
+  // Height: Number,
+  // Weight: Number,
+  // BP: Number,
+  // Smocking: Boolean,
+  // Alchohol:Boolean
+});
 
-module.exports = patientSchema;
+const url =
+  "mongodb+srv://admin:admin@cluster0.aoulm42.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+patientSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Patient", patientSchema);
