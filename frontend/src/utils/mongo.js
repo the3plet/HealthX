bfeaa9209215a0deb8144539a0233867aea23095
Sreedeep CV <sreedeep200@lbscek.ac.in>
2022-11-20 04:33:12 +0530

    Setup connection partially from mongo to backend

--- frontend/src/utils/mongo.js
+++ frontend/src/utils/mongo.js
@@ -1,9 +1,27 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3003/patient/'
 
-const newPatient = (patient) =>{
-    const req = axios.post(baseUrl,patient);
-    return req.then(res=>res.data)
+const newPatient = (patient) => {
+    const req = axios.post(baseUrl, patient);
+    return req.then(res => res.data)
 }
 
-export default newPatient;
+const patientList = () => {
+    const req = axios.get(baseUrl)
+    return req.then(res => res.data)
+}
+
+const singlePerson = (id) => {
+    const req = axios.get(`${baseUrl}/${id}`);
+    return req.then(res => res.data)
+}
+
+const removepatient = (id) => {
+    const req = axios.delete(`${baseUrl}/${id}`);
+    return req.then((res) => res.data);
+};
+
+const exports = { patientList, newPatient, removepatient, singlePerson };
+
+// export default newPatient;
+export default exports;
