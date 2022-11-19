69a60340d750e75058fe0cd4b993e70b4b9914b9
Sreedeep CV <sreedeep200@lbscek.ac.in>
2022-11-20 00:50:29 +0530

    Added database apis

--- healthx-backend/controllers/patients.js
+++ healthx-backend/controllers/patients.js
@@ -1,17 +1,60 @@
-const patientsRouter = require('express').Router()
-const Patient = require('../models/patient')
+const patientsRouter = require("express").Router();
+const { default: next } = require("next");
+const Patient = require("../models/patient");
 
-patientsRouter.get('/info',(req,res)=>{
-    res.set('content-type','text/html')
-    res.send(`<p>Many people present</p>`)
-})
+patientsRouter.use(function (req, res, next) {
+    req.start = new Date(Date.now());
+    next();
+});
 
-patientsRouter.get('/:id',(req,res)=>{
-    res.send(`Patient-${req.params.id}`)
-})
+// all patient info
+patientsRouter.get("/", (req, res) => {
+    Patient.find({})
+        .then((patients) => res.json(patients))
+        .catch((err) => next(err));
+});
+// total patient info
+patientsRouter.get("/info", (req, res) => {
+    res.set("content-type", "text/html");
+    Patient.find({})
+        .then((patients) => res.send(`<p>HelthX has info of ${patients.length} patient</p>`))
+});
 
-patientsRouter.post('/',(req,res)=>{
-const body = req.body;
-})
+// get a person by ID
+patientsRouter.get("/:id", (req, res, next) => {
+    Patient.findById(req.params.id)
+        .then((patient) => res.json(patient))
+        .catch((err) => next(err));
+});
+//delete a patient using ID
+patientsRouter.delete("/:id", (req, res, next) => {
+    Patient.findByIdAndRemove(req.params.id)
+        .then(() => res.status(204).end())
+        .catch((err) => next(err));
+});
+//inset data
+patientsRouter.post("/", (req, res, next) => {
+    const body = req.body;
 
-module.exports = patientsRouter
+    if (!body.name) {
+        return res.status(400).json({
+            error: "name is missing",
+        });
+    } else if (!body.age) {
+        return res.status(400).json({
+            error: "number is missing",
+        });
+    }
+
+    const patient = new Patient({
+        name: body.name,
+        age: body.age,
+    });
+
+    patient
+        .save()
+        .then((addedPatient) => res.json(addedPatient))
+        .catch((err) => next(err));
+});
+
+module.exports = patientsRouter;
--- healthx-backend/models/patient.js
+++ healthx-backend/models/patient.js
@@ -1,14 +1,34 @@
 const mongoose = require("mongoose")
-
+const url = 'mongodb+srv://admin:admin@cluster0.aoulm42.mongodb.net/?retryWrites=true&w=majority'
 const patientSchema = new mongoose.Schema({
     name: String,
-    Sex: String,
     Age: Number,
-    Height: Number,
-    Weight: Number,
-    BP: Number,
-    Smocking: Boolean,
-    Alchohol:Boolean
+    // Height: Number,
+    // Weight: Number,
+    // Sex: String,
+    // BP: Number,
+    // Smoking: Boolean,
+    // Alchohol: Boolean
 })
 
-module.exports = patientSchema;
+console.log('connecting to', url)
+
+mongoose
+    .connect(url)
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+
+patientSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    },
+})
+
+module.exports = mongoose.model('Patient', patientSchema)
+// module.exports = patientSchema;
