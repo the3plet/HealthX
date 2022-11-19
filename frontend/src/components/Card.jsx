3a28b19259a79ce8459dbcb6262856f2f6ab6b34
the3plet <soorajp2002@gmail.com>
2022-11-20 00:11:17 +0530

    Patient Profile card

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -12,39 +12,57 @@ import {
   Stack,
   TextField,
   Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
 } from "@mui/material";
 import CardActions from "@mui/material/CardActions";
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
-  >
-    •
-  </Box>
-);
+import { color } from '@mui/system';
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
+const patients = [
+  { avatar: null, name: "Smoking Bool" },
+  { avatar: null, name: "Alcohol Bool" },
+  { avatar: null, name: "BMI" },
+  { avatar: null, name: "Health Risk" },
+  { avatar: null, name: "Diet" },
+  { avatar: null, name: "Activity" },
+];
 
 export default function BasicCard() {
   return (
-    <Card sx={{ minWidth: 275 }}>
+    <Card sx={{ minWidth: 275, backgroundColor: "#eeeeee",mt:"10px" }}>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        <TableContainer>
+          <Table sx={{ minWidth: 500 }}>
+            <TableHead >
+              <TableRow>
+                <Typography variant="h6">Report</Typography>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {patients.map((patient) => {
+                return (
+                  <TableRow key={patient.name}>
+                    <TableCell sx={{ py: 1, px: 1 }}>
+                      <span style={{ display: "flex", alignItems: "center" }}>
+                        <AccountCircleIcon
+                          sx={{ width: "2rem", height: "2rem", mr: 1 }}
+                        />
+                        {patient.name}
+                      </span>
+                    </TableCell>
+                    
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
   );
 }
